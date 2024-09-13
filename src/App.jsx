dea22167543c6eed296aa25712866ef5f2b02e96
= <ngstafford1@gmail.com>
2024-09-13 10:32:47 -0400

    added react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
     <div>
       <h1>Hello World</h1>
       <h2>Nicholas Stafford. Full Stack Developer.</h2>
-      <p>Test</p>
       <motion.div
         className="box"
         initial={{ obacity: 0, scale: 0 }}
@@ -23,17 +22,38 @@ function App() {
         <li><a>Icebox</a></li>
       </ul>
       <h1>Your next website</h1>
-      <h1>About me</h1>
+      <p>Contact my business email at ngstafford1@gmail.com.</p>
       <h1>Education</h1>
       <h2>Masters of Computer Science - Kent State University</h2>
       <p>In progress. I am a graduate student at Kent State. Currently studying Cryptography and AI.</p>
       <h2>B.S Computer Science. GPA: 3.958 - Kent State University</h2>
       <p>You have no idea how much that A- in Latin class still urks me.</p>
-      <h1>Projects</h1>
+      <h1>Technology</h1>
+      <h2>Languages</h2>
+      <ul>
+        <li>Typescript</li>
+        <li>Javascript</li>
+        <li>C++</li>
+        <li>Python</li>
+      </ul>
+      <h2>Frameworks</h2>
+      <ul>
+        <li>AngularJS</li>
+        <li>Angular</li>
+        <li>React</li>
+        <li>Flask</li>
+      </ul>
+      <h2>Environment</h2>
+      <p>I am a big fan of Neovim.</p>
+      <p>I primarily use Linux</p>
+      <h1>Open Source</h1>
+      <p>I love code. Below is a selection of some of my pet projects.</p>
       <h2>Visual NPM</h2>
       <p>Visualize the tools that power the web. See it change over time. Coming soon. . .</p>
       <h2>NBA Goat Calculator</h2>
       <p>Generations have argued. Is anyone right? Who you consider the greatest depends on your values. This app asks what you value, and with the power of advanced analytics, ranks players accordingly. Coming soon. . .</p>
+      <h2>Neovim Config</h2>
+      <p>Link</p>
     </div>
   );
   /*
