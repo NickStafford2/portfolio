e525017efacd2db13cdadc90eb700ee4161713b7
Nick <ngstafford1@gmail.com>
2024-10-05 17:42:45 -0400

    fixed header

--- src/template/Header.jsx
+++ src/template/Header.jsx
@@ -1,29 +1,35 @@
-import { motion, useMotionValue, useMotionValueEvent, useScroll, useTransform } from "framer-motion"
+import {
+  motion,
+  useMotionValue,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "framer-motion";
 
 import { useRef, useState } from "react";
-function Header() {
+import "./Header.css";
 
-  const [yText, setYText] = useState("10em");
+function Header() {
+  const [yText, setYText] = useState("40px");
   const [y, setY] = useState(10);
 
   const inputRef = useRef(null);
-  const { scrollYProgress, scrollY } = useScroll()
+  const { scrollYProgress, scrollY } = useScroll();
   // useScroll({
   //   target: ref,
   //   offset: ["end end", "start start"]
   // });
   const handleInputChangeY = () => {
     setYText(y + "em");
-    console.log(yText)
+    // console.log(yText);
   };
   // const rotate = useTransform(scrollYProgress, [0, 1], [20, 0]);
-  const translate = useTransform(scrollYProgress, [0, 1], [0, 100])
-  const x = useMotionValue(0)
+  // const translate = useTransform(scrollYProgress, [0, 1], [0, 100]);
   useMotionValueEvent(scrollY, "change", (latest) => {
-    console.log("Page scroll: ", latest)
-    let size = Math.min(50, 600 - latest)
-    setYText(size + "px")
-  })
+    console.log("Page scroll: ", latest);
+    let size = Math.min(40, 600 - latest) / 2;
+    setYText(size + "px");
+  });
   // useMotionValueEvent(x, "animationStart", () => {
   //   console.log("animation started on x")
   // })
@@ -33,34 +39,25 @@ function Header() {
   // })
   return (
     <>
-
-      <motion.nav className="page-width"
-        animate={{ paddingTop: yText }}
-        // transition={{ ease: "easeInOut", duration: 3 }}
-        style={{
-          top: 0,
-          position: "fixed",
-          backgroundColor: "tan",
-          // translateX: translate,
-          // rotateX: rotate, // rotate in X-axis
-          // position: "absolute"
-        }}
+      <motion.nav
+        className="header"
+        animate={{ paddingTop: yText, paddingBottom: yText }}
+        // transition={{ ease: "easeInOut", duration: 0.3 }}
+        style={
+          {
+            // translateX: translate,
+            // rotateX: rotate, // rotate in X-axis
+            // position: "absolute"
+          }
+        }
       >
+        <h3 className="header-title">Nicholas Stafford</h3>
         <div className="gradient-box">
           <a>RESUME</a>
         </div>
       </motion.nav>
-      <div
-        style={{
-          paddingTop: "80vh"
-        }}
-      // ref={ref}
-      >
-        anchor
-        {/* {{ scrollYProgress }} */}
-      </div>
     </>
   );
 }
 
-export default Header
+export default Header;
