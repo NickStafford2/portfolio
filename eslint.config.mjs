6a7661a6a755c80e3e285ac31869bfc1f38caa61
Nick <ngstafford1@gmail.com>
2024-10-06 11:16:04 -0400

    fixed parcer

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,12 @@ import react from 'eslint-plugin-react'
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
 import reactHooks from 'eslint-plugin-react-hooks'
 import globals from 'globals'
-import tsParser from '@typescript-eslint/parser'
+// import tsParser from '@typescript-eslint/parser'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
 import { FlatCompat } from '@eslint/eslintrc'
+import tsParser from '@typescript-eslint/eslint-plugin'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -28,6 +29,7 @@ export default [
         )
     ),
     {
+        // root: true,
         plugins: {
             react: fixupPluginRules(react),
             '@typescript-eslint': fixupPluginRules(typescriptEslint),
@@ -52,14 +54,13 @@ export default [
         },
 
         settings: {
-
-       'import/resolver': {
-            node: {
-                extensions: ['.js', '.jsx', '.ts', '.tsx'],
-                moduleDirectory: ['node_modules', 'src/'],
+            'import/resolver': {
+                node: {
+                    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+                    moduleDirectory: ['node_modules', 'src/'],
+                },
             },
         },
-
         rules: {
             'no-use-before-define': 'off',
             '@typescript-eslint/no-use-before-define': ['error'],
@@ -113,4 +114,3 @@ export default [
         },
     },
 ]
-
