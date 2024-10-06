dfae13aeda12a83d74f195e712e05023f8f97488
Nick <ngstafford1@gmail.com>
2024-10-06 16:23:49 -0400

    added shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,30 @@
-import { StrictMode, useState } from "react";
-import * as ReactDOMClient from "react-dom/client";
+import { StrictMode, useState } from 'react'
+import * as ReactDOMClient from 'react-dom/client'
 
-import App from "./App";
-import Header from "./template/Header";
-import Sidebar from "./template/Sidebar";
-import { Footer } from "./template/Footer";
-import { Refresh } from "./template/Refresh";
+// import App from './App'
+// import Header from './template/Header'
+import Sidebar from './template/Sidebar'
+// import { Footer } from './template/Footer'
 //import "@fontsource/poppins" // Defaults to weight 400 with all styles included.
-import "@fontsource-variable/raleway";
+import '@fontsource-variable/raleway'
 
 function AppWithUI() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
 
-  return (
-    <StrictMode>
-      <Header></Header>
-      <div className="page">
-        <Sidebar></Sidebar>
-        <App key={count} className="" />
-      </div>
-    </StrictMode>
-  );
+    return (
+        <StrictMode>
+            {/* <Header></Header> */}
+            <div className="page">
+                <Sidebar></Sidebar>
+                {/* <App key={count} /> */}
+            </div>
+        </StrictMode>
+    )
 }
 
-const rootElement = document.getElementById("root");
-const root = ReactDOMClient.createRoot(rootElement);
-root.render(<AppWithUI />);
+const rootElement = document.getElementById('root')
+const root = ReactDOMClient.createRoot(rootElement)
+root.render(<AppWithUI />)
 
 /*import { StrictMode, useState } from "react";
 import * as ReactDOMClient from "react-dom/client";
--- src/template/Sidebar.tsx
+++ src/template/Sidebar.tsx
@@ -1,26 +1,19 @@
-import { motion } from "framer-motion"
-import "./Sidebar.css"
-
+import { motion } from 'framer-motion'
+import './Sidebar.css'
 
 function Sidebar() {
-
-  return (
-
-    <motion.div
-      className="sidebar"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 3 }}
-    >
-      <div className="sidebar-spacer"></div>
-      <div className="sidebar-item">Me</div>
-      <div className="sidebar-item">Education</div>
-      <div className="sidebar-item">Skills</div>
-      <div className="sidebar-item">Projects</div>
-      <div className="sidebar-item">Beyond Code</div>
-      <div className="sidebar-item">Contact</div>
-    </motion.div>
-  );
+    return (
+        <div>
+            className="sidebar"
+            <div className="sidebar-spacer"></div>
+            <div className="sidebar-item">Me</div>
+            <div className="sidebar-item">Education</div>
+            <div className="sidebar-item">Skills</div>
+            <div className="sidebar-item">Projects</div>
+            <div className="sidebar-item">Beyond Code</div>
+            <div className="sidebar-item">Contact</div>
+        </div>
+    )
 }
 
 export default Sidebar
