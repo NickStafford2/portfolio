c44ebd1e05ee6da1408b7d775bba64e37caa73df
Nick <ngstafford1@gmail.com>
2024-10-08 01:43:22 -0400

    started adding library components

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -3,6 +3,8 @@ import { StrictMode } from 'react'
 import Header from './template/Header'
 import Sidebar from './template/Sidebar'
 
+import BackgroundGrid from '@/components/edil-ozi/background-grid'
+
 export function Portfolio() {
   return (
     <StrictMode>
@@ -10,6 +12,13 @@ export function Portfolio() {
       <div className="page">
         <Sidebar></Sidebar>
         <App />
+
+        <section
+          className="fixed top-0 bottom-0 left-0 right-0 bg-black dark:bg-black"
+          style={{ zIndex: -999 }}
+        >
+          <BackgroundGrid />
+        </section>
       </div>
     </StrictMode>
   )
