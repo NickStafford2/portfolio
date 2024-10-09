d7cff4dc7da040a2e73cfb6b8b0acc59099abecc
= <ngstafford1@gmail.com>
2024-10-09 11:47:25 -0400

    added rotate between quotes

--- src/template/QuoteBox.tsx
+++ src/template/QuoteBox.tsx
@@ -1,15 +1,14 @@
+import RotateBetween from '@/components/edil-ozi/rotate-between'
+
 export function QuoteBox() {
   const quotes = [
     "If a programming language does not make you think. It isn't worth using.",
     'An eye for an eye makes the whole world blind.',
-    'talk softly and carry a big stick',
+    'Talk softly and carry a big stick',
   ]
   return (
     <div className="h-screen">
-      <h1>Keep in mind</h1>
-      {quotes.map((quote) => (
-        <p>{quote}</p>
-      ))}
+      <RotateBetween words={quotes} className="text-2xl"></RotateBetween>
     </div>
   )
 }
