dcafd9fc7fd0da9f533a6b1743f270c4e76cc3bc
= <ngstafford1@gmail.com>
2024-10-09 17:48:35 -0400

    added css to hero with arrows

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,11 +1,11 @@
 import TextWriter from '@/components/uiBeats/TextWriter'
-import { FaChevronDown } from 'react-icons/fa'
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa'
 
 export function Hero() {
   return (
     <div
       id="hero"
-      className="min-h-screen bg-gradient-to-t from-cyan-50 to-gray-700 flex justify-center flex-col"
+      className="min-h-screen bg-opacity-20 flex justify-center flex-col"
     >
       <TextWriter text="Hello World" className="text-8xl mb-6" delay={0.2} />
       <div className="max-w-[600px] ">
@@ -18,22 +18,21 @@ export function Hero() {
           delay={0.05}
         />
       </div>
-      <div className="flex flex-row w-full py-10 justify-between pr-60">
-        <div className="p-1">
+      <div className="flex flex-row w-full py-10 justify-between  items-center bg-opacity-30 bg-gray-600 ">
+        <div className="flex flex-col flex-grow justify-center align-middle ">
           <a href="#education">
             <span className="text-5xl">Learn More:</span>
             <br />
-            <FaChevronDown className="text-8xl"></FaChevronDown>{' '}
+            <FaChevronDown className="text-[10em] opacity-30"></FaChevronDown>{' '}
           </a>
         </div>
-        <div className="p-1">
-          <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-10 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-              Contact me
-            </span>
-          </button>
-          <p>Contact my business email at ngstafford1@gmail.com.</p>
+        <span className="text-4xl align-center">- or -</span>
+        <div className="flex flex-col flex-grow ">
+          <a href="#" className="flex flex-col items-end align-bottom">
+            <FaChevronUp className="text-[10em] opacity-30"></FaChevronUp>{' '}
+            <br />
+            <span className="text-5xl">Contact Me</span>
+          </a>
         </div>
       </div>
     </div>
