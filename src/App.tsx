84be96e96fe2016684fba2aee2030f5c7f66187a
= <ngstafford1@gmail.com>
2024-10-09 09:39:54 -0400

    refactored projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Hero } from './Hero'
 import { Education } from './template/Education'
+import { Projects } from './template/Projects'
 import { Skills } from './template/Skills'
 
 // idea. Make a terminal looking thing. use react to change text over time like someone is typing.
@@ -10,37 +11,7 @@ function App() {
       <Hero></Hero>
       <Skills></Skills>
       <Education></Education>
-      <div className="section">
-        <h1>Pet Projects</h1>
-        <p>
-          I love code. Below is a selection of some stuff I am working on in my
-          spare time.
-        </p>
-        <h2>Visual NPM</h2>
-        <p>
-          Visualize the tools that power the web. See it change over time.
-          Coming soon. . .
-        </p>
-        <h2>NBA Goat Calculator</h2>
-        <p>
-          Generations have argued. Is anyone right? Who you consider the
-          greatest depends on your values. This app asks what you value, and
-          with the power of advanced analytics, ranks players accordingly.
-          Coming soon. . .
-        </p>
-        <p>
-          I am returning to the church of vi after many years away. It is
-          glorious. I don't want to use the mouse ever again. I have spent way
-          too many hours configuring my neovim setup. I started with
-          kickstarter, then I ruined that configuration. Now I switched to my
-          own customized version of AstroVim. Week by week, I will morph this
-          beast into my perfect IDE.
-        </p>
-        <div>
-          Insert image of google search "vi: did you mean emacs?" and the
-          reverse
-        </div>
-      </div>
+      <Projects></Projects>
       <div className="section">
         <h1>Keep in mind</h1>
         <p className="quote-box">
---
+++ src/template/Projects.tsx
@@ -0,0 +1,32 @@
+export function Projects() {
+  return (
+    <div className="section">
+      <h1>Pet Projects</h1>
+      <p>
+        I love code. Below is a selection of some stuff I am working on in my
+        spare time.
+      </p>
+      <h2>Visual NPM</h2>
+      <p>
+        Visualize the tools that power the web. See it change over time. Coming
+        soon. . .
+      </p>
+      <h2>NBA Goat Calculator</h2>
+      <p>
+        Generations have argued. Is anyone right? Who you consider the greatest
+        depends on your values. This app asks what you value, and with the power
+        of advanced analytics, ranks players accordingly. Coming soon. . .
+      </p>
+      <p>
+        I am returning to the church of vi after many years away. It is
+        glorious. I don't want to use the mouse ever again. I have spent way too
+        many hours configuring my neovim setup. I started with kickstarter, then
+        I ruined that configuration. Now I switched to my own customized version
+        of AstroVim. Week by week, I will morph this beast into my perfect IDE.
+      </p>
+      <div>
+        Insert image of google search "vi: did you mean emacs?" and the reverse
+      </div>
+    </div>
+  )
+}
