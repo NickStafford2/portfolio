20c7ab2f1b762c7e0c24472def122f9d16a5b564
= <ngstafford1@gmail.com>
2024-10-09 16:48:44 -0400

    changed header

--- src/template/Header.tsx
+++ src/template/Header.tsx
@@ -10,62 +10,64 @@ import {
   IconTerminal2,
 } from '@tabler/icons-react'
 
+const links = [
+  {
+    title: 'Home',
+    icon: (
+      <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: '#home',
+  },
+
+  {
+    title: 'Products',
+    icon: (
+      <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: '#skills',
+  },
+
+  {
+    title: 'Components',
+    icon: (
+      <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: '#',
+  },
+
+  {
+    title: 'Beyond Code',
+    icon: (
+      <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: '#beyond-code',
+  },
+
+  {
+    title: 'Twitter',
+    icon: (
+      <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: '#',
+  },
+  {
+    title: 'GitHub',
+    icon: (
+      <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: '#',
+  },
+]
+
 export default function Header() {
-  const links = [
-    {
-      title: 'Home',
-      icon: (
-        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: '#home',
-    },
-
-    {
-      title: 'Products',
-      icon: (
-        <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: '#skills',
-    },
-
-    {
-      title: 'Components',
-      icon: (
-        <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: '#',
-    },
-
-    {
-      title: 'Beyond Code',
-      icon: (
-        <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: '#beyond-code',
-    },
-
-    {
-      title: 'Twitter',
-      icon: (
-        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: '#',
-    },
-    {
-      title: 'GitHub',
-      icon: (
-        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: '#',
-    },
-  ]
   return (
-    <div className="fixed flex items-center justify-center w-full bg-gray-800 z-30">
+    <div className="fixed flex items-center justify-center w-full bg-gray-800 z-30 gap-3">
+      <h1 className="text-2xl">Nicholas Stafford</h1>
+      <FloatingDock items={links} />
       <Button>
         <FaRegSnowflake className="w-7 h-6" />
         <span className="pr-4">Resume</span>
       </Button>
-      <FloatingDock items={links} />
       <Button>
         <span className="pr-4">Icebox Login</span>
         <FaRegSnowflake className="w-7 h-6" />
