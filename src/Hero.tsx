43242837f9d7b76f97867fac2437f9b91b73672a
= <ngstafford1@gmail.com>
2024-10-09 16:28:18 -0400

    adding better into to hero

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,4 +1,5 @@
 import TextWriter from '@/components/uiBeats/TextWriter'
+import { Button } from './components/ui/button'
 
 export function Hero() {
   return (
@@ -6,22 +7,42 @@ export function Hero() {
       id="hero"
       className="min-h-screen bg-gradient-to-t from-cyan-50 to-gray-700 flex justify-center flex-col"
     >
-      <h1 className="text-8xl font-extrabold py-32">Nicholas Stafford</h1>
+      <TextWriter text="Hello World" className="text-8xl mb-6" delay={0.2} />
+      <TextWriter
+        text="
+        My name is Nicholas Stafford. I am a web developer based in Cleveland,
+        Ohio. Let me tell you a bit about myself. . .
+      "
+        className="max-w-[300] text-xl mb-6"
+        delay={0.05}
+      />
+
+      <div className="flex flex-row w-full py-20">
+        <div className="p-1">
+          <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-10 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+              Learn More
+            </span>
+          </button>
+        </div>
+        <div className="p-1">
+          <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-10 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+              Contact me
+            </span>
+          </button>
+        </div>
+        <Button>Learn more</Button>
+        <Button>Learn more</Button>
+      </div>
       <TextWriter
         text="Full Stack Developer"
         className="text-3xl mb-6"
         delay={0.1}
       />
       <TextWriter text="I Freelance" className="text-3xl mb-6" delay={0.2} />
-      <div className="p-1">
-        <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-10 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-            Contact me
-          </span>
-        </button>
-      </div>
-
       <p>Contact my business email at ngstafford1@gmail.com.</p>
     </div>
   )
