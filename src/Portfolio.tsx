2cb62d834a6f0b5452fb08672024c92fb8c0fb89
= <ngstafford1@gmail.com>
2024-10-11 14:51:05 -0400

    started making framer sidebar

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,16 +1,30 @@
 import App from './App'
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import Header from './template/Header'
-// import Sidebar from './template/Sidebar'
-
+import Sidebar from './template/Sidebar'
+import { motion } from 'framer-motion'
 import BackgroundGrid from '@/components/edil-ozi/background-grid'
+import { Button } from './components/ui/button'
 
+const variants = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: '-100%' },
+}
 export function Portfolio() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <StrictMode>
       <Header></Header>
-      <div className="py-24 ">
-        {/* <Sidebar></Sidebar> */}
+      <div className="py-24 flex flex-row">
+        <Button onClick={() => setIsOpen((isOpen) => !isOpen)}>Sidebar</Button>
+        <motion.div
+          animate={isOpen ? 'open' : 'closed'}
+          variants={variants}
+          className="py-24 fixed left-0 top-[100px] bottom-0 w-[300px]"
+        >
+          <Sidebar></Sidebar>
+        </motion.div>
         <App />
 
         <section
--- src/template/Sidebar.tsx
+++ src/template/Sidebar.tsx
@@ -1,19 +1,18 @@
-import { motion } from 'framer-motion'
+// import { motion } from 'framer-motion'
 import './Sidebar.css'
 
 function Sidebar() {
-    return (
-        <div>
-            className="sidebar"
-            <div className="sidebar-spacer"></div>
-            <div className="sidebar-item">Me</div>
-            <div className="sidebar-item">Education</div>
-            <div className="sidebar-item">Skills</div>
-            <div className="sidebar-item">Projects</div>
-            <div className="sidebar-item">Beyond Code</div>
-            <div className="sidebar-item">Contact</div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="sidebar-spacer"></div>
+      <div className="sidebar-item">Me</div>
+      <div className="sidebar-item">Education</div>
+      <div className="sidebar-item">Skills</div>
+      <div className="sidebar-item">Projects</div>
+      <div className="sidebar-item">Beyond Code</div>
+      <div className="sidebar-item">Contact</div>
+    </div>
+  )
 }
 
 export default Sidebar
