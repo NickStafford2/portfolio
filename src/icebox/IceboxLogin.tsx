2d6651cdf91ca277be39719704b9acf6e1a618bf
= <ngstafford1@gmail.com>
2024-10-16 13:26:43 -0400

    added a href

--- src/icebox/IceboxLogin.tsx
+++ src/icebox/IceboxLogin.tsx
@@ -5,7 +5,16 @@ import { Label } from '@/components/ui/label'
 const IceboxLogin: FC<HTMLAttributes<HTMLDivElement>> = ({ className }) => {
   return (
     <div className={`h-full ${className}`}>
-      <p>The icebox is a private server I built. Login for remote access.</p>
+      <p>
+        The icebox is a private server I built. Login for remote access.{' '}
+        <a
+          href="#"
+          className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+        >
+          See details
+        </a>
+        .
+      </p>
       <h2 className="text-2xl pt-8">Enter Password</h2>
       <Label htmlFor="password">Password</Label>
       <Input id="password" type="password" />
