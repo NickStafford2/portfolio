23118027072043fa8769b54ff0298230f55020f9
= <ngstafford1@gmail.com>
2024-10-16 10:18:04 -0400

    added more content to self add section

--- src/template/SelfAdd.tsx
+++ src/template/SelfAdd.tsx
@@ -11,28 +11,41 @@ export default function SelfAdd() {
           className="w-full basis-1/3"
           alt="profile-picture"
         />
-        <div className="flex flex-col basis-2/3 gap-3 font-semibold">
-          <h2 className="text-3xl">Education</h2>
-          <ul className="text-xl">
-            <li className="text-xl">
-              M.S in Computer Science from Kent State University. (in progress)
-            </li>
-            <li>
-              B.S in Computer Science from Kent State University. GPA: 3.98
-            </li>
-          </ul>
-          <h2 className="pt-5 text-3xl">My Best Work</h2>
-          <div className="grid grid-cols-2 gap-3 ">
-            <NsCard
-              title="Visual NPM"
-              description="My current side project. Analyzes and visualizes NPM packages. Try it out. . . "
-            >
-              <VisualNpmDemo></VisualNpmDemo>
-            </NsCard>
-            <NsCard
-              title="InflatableOffice.com"
-              description="Hired as a web developer, I ended up working as a software architect. Successfully converted the companies main website from AngularJs to Angular."
-            ></NsCard>
+        <div className="flex flex-col justify-between basis-2/3 font-semibold">
+          <p>
+            I am a software developer. Solving problems and helping others are
+            my true passions. When I have spare time, you will find me reading
+            about some obscure subject. Ancient history, quantum mechanics, I
+            love it all.
+          </p>
+          <div>
+            <h2 className="text-3xl">Education</h2>
+            <ul className="text-xl">
+              <li className="text-xl">
+                M.S in Computer Science from Kent State University. (in
+                progress)
+              </li>
+              <li>
+                B.S in Computer Science from Kent State University. GPA: 3.98
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="pt-5 text-3xl">My Best Work</h2>
+            <div className="flex flex-col items-start gap-3 ">
+              <NsCard
+                className="min-h-36"
+                title="Visual NPM"
+                description="My current side project. Analyzes and visualizes NPM packages. Try it out. . . "
+              >
+                <VisualNpmDemo></VisualNpmDemo>
+              </NsCard>
+              <NsCard
+                className="min-h-36"
+                title="InflatableOffice.com"
+                description="Hired as a web developer, I ended up working as a software architect. Successfully converted the companies main website from AngularJs to Angular."
+              ></NsCard>
+            </div>
           </div>
         </div>
       </div>
