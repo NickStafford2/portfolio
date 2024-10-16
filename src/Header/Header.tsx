541c7fe191c1ad43d1cb2fbcfabf112f276b55d8
= <ngstafford1@gmail.com>
2024-10-16 14:12:53 -0400

    removed comment

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,85 +1,12 @@
-// import { FloatingDock } from '@/components/ui/floating-dock'
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover'
-// import { FaRegSnowflake } from 'react-icons/fa'
-// import {
-//   IconBrandGithub,
-//   IconBrandX,
-//   IconExchange,
-//   IconHome,
-//   IconNewSection,
-//   IconTerminal2,
-// } from '@tabler/icons-react'
 
 import HeaderLink from './HeaderLink.tsx'
 import IceboxLogin from '@/icebox/IceboxLogin.tsx'
 import ThemeSwitcher from './ThemeSwitcher.tsx'
-
-// const links = [
-//   {
-//     title: 'Home',
-//     icon: (
-//       <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-//     ),
-//     href: '#home',
-//   },
-//
-
-//     title: 'Experience',
-//     icon: (
-//       <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-//     ),
-//     href: '#experience',
-//   },
-//
-//   {
-//     title: 'Projects',
-//     icon: (
-//       <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-//     ),
-//     href: '#projects',
-//   },
-//
-//   {
-//     title: 'Beyond Code',
-//     icon: (
-//       <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-//     ),
-//     href: '#beyond-code',
-//   },
-//
-//   {
-//     title: 'Twitter',
-//     icon: (
-//       <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-//     ),
-//     href: '#',
-//   },
-//   {
-//     title: 'GitHub',
-//     icon: (
-//       <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-//     ),
-//     href: '#',
-//   },
-//   {
-//     title: 'resume',
-//     icon: (
-//       <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-//     ),
-//     href: '#',
-//   },
-//   {
-//     title: 'Icebox Login',
-//     icon: (
-//       <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-//     ),
-//     href: '#',
-//   },
-// ]
 
 export default function Header() {
   return (
@@ -108,7 +35,6 @@ export default function Header() {
           </PopoverContent>
         </Popover>
       </div>
-      {/* <FloatingDock items={links} /> */}
     </div>
   )
 }
