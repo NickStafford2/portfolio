69877942afd7ffb0173f139fc16b67dc90cb6d7e
= <ngstafford1@gmail.com>
2024-10-16 22:41:43 -0400

    simple rotating icons on a canvas

--- src/template/gravity/OptionTwo.tsx
+++ src/template/gravity/OptionTwo.tsx
@@ -6,43 +6,57 @@ import logoPath from '@public/logos/pythonLogo.svg'
 
 export interface ItemState {
   x: number
+  dx: number
   y: number
+  dy: number
   rotate: number
+  dr: number
 }
 export const OptionTwo: React.FC = () => {
   const canvas = useRef<HTMLCanvasElement | null>(null)
+  const fps = 2
   // Creating an imgRef to create an `Image` to draw the logo svg to
   const backgroundRef = useRef<HTMLImageElement | null>(null)
   const logoRef = useRef<HTMLImageElement | null>(null)
   // Angle in radians
   const rotationRef = useRef(0)
+  const stateRef = useRef<ItemState[]>()
 
-  const state: ItemState[] = initState()
   function initState(): ItemState[] {
-    const h = canvas.current!.height
-    const w = canvas.current!.width
     const states = []
     for (let i = 0; i < 10; i++) {
       const sign = Math.random() > 0.5 ? 1 : -1
       const s: ItemState = {
-        x: (Math.random() * h) / 2,
-        y: (Math.random() * w) / 2,
-        rotate: Math.random() * 0.2 * sign,
+        x: Math.random() * canvas.current!.width,
+        dx: Math.random() / 100,
+        y: Math.random() * canvas.current!.height,
+        dy: Math.random() / 100,
+        rotate: Math.random() * sign,
+        dr: Math.random() * 0.2,
       }
       states.push(s)
     }
     return states
   }
   const animate = () => {
-    // When the component is unmounted, we will lose the canvas.current ref, so exit early to clean up
-    if (!canvas.current) {
-      return
-    }
-    const rotation = rotationRef.current
-    draw({ canvas, backgroundRef, logoRef, rotation, state })
-    rotationRef.current = rotation + radian(0.2)
+    setTimeout(() => {
+      // When the component is unmounted, we will lose the canvas.current ref, so exit early to clean up
+      if (!canvas.current || !stateRef.current) {
+        return
+      }
+      for (const s of stateRef.current) {
+        // console.log(s)
+        s.rotate = s.rotate + s.dr
+        // s.x = s.x + s.dx
+        // s.y = s.y + s.dy
+      }
+      const rotation = rotationRef.current
+      // console.log(stateRef)
+      draw({ canvas, backgroundRef, logoRef, rotation, stateRef })
+      rotationRef.current = rotation + radian(0.2)
 
-    requestAnimationFrame(animate)
+      requestAnimationFrame(animate)
+    }, 1000 / fps)
   }
 
   useEffect(() => {
@@ -60,6 +74,8 @@ export const OptionTwo: React.FC = () => {
     logo.onload = () => {
       logoRef.current = logo
     }
+    // maybe load this once at start somehow?
+    stateRef.current = initState()
     // Start the animation function
     requestAnimationFrame(animate)
   }, [])
--- src/template/gravity/helpers.ts
+++ src/template/gravity/helpers.ts
@@ -7,29 +7,41 @@ interface DrawProps {
   logoRef: React.MutableRefObject<HTMLImageElement | null>
   backgroundRef: React.MutableRefObject<HTMLImageElement | null>
   rotation: number
-  state: ItemState[]
+  stateRef: React.MutableRefObject<ItemState[] | undefined>
 }
 export const draw = (props: DrawProps) => {
-  const { canvas, logoRef, rotation } = props
+  const { canvas, logoRef, rotation, stateRef } = props
   const ctx = canvas?.current?.getContext('2d')
   const logo = logoRef.current
-  if (!ctx || !logo) {
+  const state = stateRef.current
+  if (!ctx || !logo || !state) {
+    console.error('oh no')
+    console.log('tt')
     return
   }
   const canvasWidth = canvas.current!.width
   const canvasHeight = canvas.current!.height
+  // ctx.save()
   ctx.clearRect(0, 0, canvasWidth, canvasHeight)
 
   // Code adapted from https://stackoverflow.com/a/3793474
-
-  const x = props.state[0].x
-  const y = props.state[0].y
-  ctx.translate(x, y)
-  ctx.rotate(rotation)
-  // const { width, height } = logo
-  const width = 60
-  const height = width
-  ctx.drawImage(logo, -width / 2, -height / 2, width, height)
-  ctx.rotate(-rotation)
-  ctx.translate(-x, -y)
+  for (const s of state) {
+    ctx.save()
+    // const x = canvasWidth / 2
+    // const x = s.x
+    // const y = s.y
+    ctx.translate(s.x, s.y)
+    // ctx.translate(s.x + canvasWidth / 2, s.y + canvasHeight / 2)
+    // ctx.translate(x, y)
+    ctx.rotate(s.rotate)
+    // const { width, height } = logo
+    const width = 60
+    const height = width
+    // ctx.drawImage(logo, -width / 2, -height / 2, width, height)
+    ctx.drawImage(logo, (width / 2) * -1, (height / 2) * -1, width, height)
+    ctx.rotate(-rotation)
+    // ctx.translate(-x, -y)
+    ctx.restore()
+  }
+  // ctx.restore()
 }
