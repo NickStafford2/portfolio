6bb8b54f46c502703d605afcd29860cd4c94d9df
= <ngstafford1@gmail.com>
2024-10-17 21:43:05 -0400

    backend dev server serves build index.html

--- backend/nsportfoliobackend/__init__.py
+++ backend/nsportfoliobackend/__init__.py
@@ -1,10 +1,16 @@
 import time
 
-from flask import Flask
+from flask import Flask, send_from_directory
 
 
 def create_app():
     app = Flask(__name__)
+
+    app = Flask(__name__, static_folder="../../dist/", static_url_path="/")
+
+    @app.route("/")
+    def index():
+        return app.send_static_file("index.html")
 
     @app.route("/time")
     def get_current_time():
