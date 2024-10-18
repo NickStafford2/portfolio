6173fde64b7d6c0eae003b3cf178e54a6b4669eb
Nick <ngstafford1@gmail.com>
2024-10-18 01:50:29 -0400

    changed text speed

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -24,7 +24,7 @@ export function Hero() {
         Ohio. Let me tell you a bit about myself. . .
       "
               className="p-3 text-2xl font-semibold text-background"
-              delay={0.05}
+              delay={0.01}
             />
           </div>
         </div>
