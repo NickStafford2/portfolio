e2499ca5a4d3248f35402add9af089891d96cacc
= <ngstafford1@gmail.com>
2024-10-21 15:40:49 -0400

    fixed name

--- src/components/ns/nsCard.tsx
+++ src/components/ns/nsCard.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/lib/utils'
 import React from 'react'
 import { motion } from 'framer-motion'
 
-export const BackgroundGradient = ({
+export const NsCard = ({
   children,
   className,
   containerClassName,
