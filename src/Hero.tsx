4a7b8aea05b91da309a6568dceb2751f319a99d0
= <ngstafford1@gmail.com>
2024-10-21 17:49:36 -0400

    New hero text

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -15,16 +15,18 @@ export function Hero() {
       <div className="flex flex-grow flex-row justify-between">
         <div className="flex flex-col justify-between">
           <TextWriter
-            text="Hello World"
-            className="text-text h-32 bg-gradient-to-t from-border to-black bg-clip-text px-6 py-0 text-8xl font-bold text-transparent shadow-[0_35px_45px_-35px_rgba(127,229,255,0.9),inset_0_-55px_35px_-35px_rgba(0,0,0,0.9)]"
-            delay={0.1}
+            // text="Allow me to introduce myself"
+            text="Nicholas Stafford"
+            className="text-text h-32 bg-gradient-to-t from-border to-white bg-clip-text px-6 py-0 text-8xl font-light text-transparent"
+            delay={0.05}
           />
           <div className="mb-6 mt-0 min-h-32 max-w-[600px] px-6">
             <TextWriter
+              // text="My name is Nicholas Stafford. I write code and make cool things. "
               text="
-        My name is Nicholas Stafford. I am a developer based in Cleveland,
-        Ohio. Let me tell you a bit about myself. . .
-      "
+                 I am a developer based in Cleveland,
+                 Ohio. Let me tell you a bit about myself. . .
+               "
               className="bg-clip-text p-3 text-2xl font-medium text-foreground"
               delay={0.01}
             />
