ec6d46488b3792ba0603b7fdd7757ad365d45189
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-08 11:25:28 -0500

    changed look of the hero section

--- src/Header/HeaderLink.tsx
+++ src/Header/HeaderLink.tsx
@@ -1,24 +1,24 @@
-import * as React from 'react'
+import * as React from "react";
 
-import { cn } from '@/lib/utils'
+import { cn } from "@/lib/utils";
 
 export interface HeaderLinkProps
-  extends React.LinkHTMLAttributes<HTMLAnchorElement> {}
+	extends React.LinkHTMLAttributes<HTMLAnchorElement> {}
 
 const HeaderLink = React.forwardRef<HTMLAnchorElement, HeaderLinkProps>(
-  ({ className, children, ...props }, ref) => {
-    return (
-      <a
-        className={cn(
-          'h-full min-w-28 text-[var(--ns-primary)] border-border border-r-[1px] first:border-l-[1px] font-semibold flex flex-row justify-center items-center hover:bg-[var(--ns-primary)] hover:text-black transition-[background-color] hover:duration-0 duration-700',
-          className
-        )}
-        ref={ref}
-        {...props}
-      >
-        <span className="">{children}</span>
-      </a>
-    )
-  }
-)
-export default HeaderLink
+	({ className, children, ...props }, ref) => {
+		return (
+			<a
+				className={cn(
+					"h-full min-w-28 text-[var(--ns-primary)] border-border border-r-[1px] first:border-l-[1px] font-semibold flex flex-row justify-center items-center hover:bg-[var(--ns-primary)] hover:text-black transition-[background-color] hover:duration-0 duration-700",
+					className,
+				)}
+				ref={ref}
+				{...props}
+			>
+				<span className="">{children}</span>
+			</a>
+		);
+	},
+);
+export default HeaderLink;
--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,8 +1,8 @@
 import TextWriter from "@/components/uiBeats/TextWriter.tsx";
-import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { BiChevronDown, BiChevronUp } from "react-icons/bi";
 // import { motion } from 'framer-motion'
 // import React from 'react'
-import ThemeSwitch from "./ThemeSwitch";
+// import ThemeSwitch from "./ThemeSwitch";
 
 import "./HeroBackgroundStars.css";
 
@@ -10,7 +10,7 @@ export function Hero() {
 	return (
 		<div
 			id="hero"
-			className="flex min-h-[calc(100vh-64px)] flex-col justify-between bg-opacity-20 pt-[60px]"
+			className="flex min-h-[calc(100vh-64px)] flex-col justify-between bg-opacity-20 pt-[60px] bg-red-300"
 		>
 			<div className="flex flex-grow flex-row justify-between">
 				<div className="flex flex-col justify-between">
@@ -18,16 +18,16 @@ export function Hero() {
 						<TextWriter
 							text="Hello World"
 							// className="text-text h-32 bg-gradient-to-t from-border to-black bg-clip-text px-6 py-0 text-8xl font-bold text-transparent shadow-[0_55px_65px_-25px_rgba(127,229,255,0.9)]"
-							className="text-text h-32 bg-gradient-to-t from-border to-black bg-clip-text px-6 py-0 text-8xl font-bold text-transparent"
+							className="text-text h-32 bg-gradient-to-t from-border to-black bg-clip-text py-0 text-8xl font-bold text-transparent"
 							delay={0.1}
 						/>
 					</div>
-					<div className="mb-6 mt-0 min-h-32 max-w-[600px] px-6">
+					<div className="mb-6 mt-0 min-h-32 max-w-[600px]">
 						<TextWriter
 							text="
-        My name is Nicholas Stafford. I am a developer based in Cleveland,
-        Ohio. Let me tell you a bit about myself. . .
-      "
+                My name is Nicholas Stafford. I am a developer based in Cleveland,
+                Ohio. Let me tell you a bit about myself. . .
+              "
 							className="bg-clip-text p-3 text-2xl font-medium text-foreground"
 							delay={0.01}
 						/>
@@ -35,22 +35,26 @@ export function Hero() {
 				</div>
 				{/* <ThemeSwitch></ThemeSwitch> */}
 			</div>
-			<div className="flex flex-row items-center justify-between py-10">
-				<div className="flex flex-grow flex-col justify-center align-middle">
-					<a href="#experience">
-						<span className="text-text text-3xl">Learn More:</span>
-						<br />
-						<FaChevronDown className="text-text text-[8em] opacity-30"></FaChevronDown>{" "}
-					</a>
-				</div>
+			<div className="flex flex-row items-center justify-between py-10  font-light ">
+				<a
+					href="#experience"
+					className="flex flex-row justify-start flex-grow hover:bg-primary"
+				>
+					<div className="flex flex-col justify-center items-center ">
+						<span className="text-text text-3xl font-light ">Learn More:</span>
+						<BiChevronDown className="text-text text-[12em] "></BiChevronDown>{" "}
+					</div>
+				</a>
 				<span className="align-center text-text text-3xl">- or -</span>
-				<div className="flex flex-grow flex-col">
-					<a href="#" className="flex flex-col items-end align-bottom">
-						<FaChevronUp className="text-text text-[8em] opacity-30"></FaChevronUp>{" "}
-						<br />
+				<a
+					href="#"
+					className="flex flex-row justify-end flex-grow hover:bg-primary"
+				>
+					<div className="flex flex-col justify-center items-center ">
+						<BiChevronUp className="text-text text-[12em] "></BiChevronUp>{" "}
 						<span className="text-text text-3xl">Contact Me</span>
-					</a>
-				</div>
+					</div>
+				</a>
 			</div>
 		</div>
 	);
