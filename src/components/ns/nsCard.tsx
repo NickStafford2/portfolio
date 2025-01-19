c734ebb8b7ee9fef0469347e02b3630ce2919424
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-18 19:15:09 -0500

    added intersection observer. performance is much better

--- src/components/ns/nsCard.tsx
+++ src/components/ns/nsCard.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
-
+import { useInView } from "react-intersection-observer";
 const borderVariants = {
 	initial: {
 		backgroundPosition: "0 50%",
@@ -26,57 +26,64 @@ export const NsCard = ({
 	showImage?: boolean;
 	id?: string;
 }) => {
-	// useOutsideClick(ref, () => setIsVisible(false))
-
+	const { ref, inView, entry } = useInView({
+		/* Optional options */
+		rootMargin: "1000px 0px 1000px 0px",
+		threshold: 0,
+	});
 	return (
 		<motion.div
 			id={id}
+			ref={ref}
 			// onClick={() => setIsVisible(!isVisible)}
 			className={cn("group relative min-h[50px] w-full p-[2px]", className)}
 		>
-			<motion.div
-				variants={animate ? borderVariants : undefined}
-				initial={animate ? "initial" : undefined}
-				animate={animate ? "animate" : undefined}
-				transition={
-					animate
-						? {
-								duration: 5,
-								repeat: Infinity,
-								repeatType: "reverse",
-							}
-						: undefined
-				}
-				style={{
-					backgroundSize: animate ? "400% 400%" : undefined,
-				}}
-				className={cn(
-					"absolute inset-0 z-[1] rounded-3xl opacity-60 blur-xl transition duration-500 will-change-transform group-hover:opacity-100",
-					"bg-[radial-gradient(circle_farthest-side_at_0_100%,var(--ns-accent),transparent),radial-gradient(circle_farthest-side_at_100%_0,var(--ns-primary),transparent),radial-gradient(circle_farthest-side_at_100%_100%,var(--ns-primary),transparent),radial-gradient(circle_farthest-side_at_0_0,var(--ns-accent),#141316)]",
-				)}
-			/>
-			<motion.div
-				variants={animate ? borderVariants : undefined}
-				initial={animate ? "initial" : undefined}
-				animate={animate ? "animate" : undefined}
-				transition={
-					animate
-						? {
-								duration: 5,
-								repeat: Infinity,
-								repeatType: "reverse",
-							}
-						: undefined
-				}
-				style={{
-					backgroundSize: animate ? "400% 400%" : undefined,
-				}}
-				className={cn(
-					"absolute inset-0 z-[1] rounded-3xl will-change-transform",
-					"bg-[radial-gradient(circle_farthest-side_at_0_100%,var(--ns-accent),transparent),radial-gradient(circle_farthest-side_at_100%_0,var(--ns-primary),transparent),radial-gradient(circle_farthest-side_at_100%_100%,var(--ns-primary),transparent),radial-gradient(circle_farthest-side_at_0_0,var(--ns-accent),#141316)]",
-				)}
-			/>
-
+			{inView && (
+				<>
+					<motion.div
+						variants={animate ? borderVariants : undefined}
+						initial={animate ? "initial" : undefined}
+						animate={animate ? "animate" : undefined}
+						transition={
+							animate
+								? {
+										duration: 5,
+										repeat: Infinity,
+										repeatType: "reverse",
+									}
+								: undefined
+						}
+						style={{
+							backgroundSize: animate ? "400% 400%" : undefined,
+						}}
+						className={cn(
+							"absolute inset-0 z-[1] rounded-3xl opacity-60 blur-xl transition duration-500 will-change-transform group-hover:opacity-100",
+							"bg-[radial-gradient(circle_farthest-side_at_0_100%,var(--ns-accent),transparent),radial-gradient(circle_farthest-side_at_100%_0,var(--ns-primary),transparent),radial-gradient(circle_farthest-side_at_100%_100%,var(--ns-primary),transparent),radial-gradient(circle_farthest-side_at_0_0,var(--ns-accent),#141316)]",
+						)}
+					/>
+					<motion.div
+						variants={animate ? borderVariants : undefined}
+						initial={animate ? "initial" : undefined}
+						animate={animate ? "animate" : undefined}
+						transition={
+							animate
+								? {
+										duration: 5,
+										repeat: Infinity,
+										repeatType: "reverse",
+									}
+								: undefined
+						}
+						style={{
+							backgroundSize: animate ? "400% 400%" : undefined,
+						}}
+						className={cn(
+							"absolute inset-0 z-[1] rounded-3xl will-change-transform",
+							"bg-[radial-gradient(circle_farthest-side_at_0_100%,var(--ns-accent),transparent),radial-gradient(circle_farthest-side_at_100%_0,var(--ns-primary),transparent),radial-gradient(circle_farthest-side_at_100%_100%,var(--ns-primary),transparent),radial-gradient(circle_farthest-side_at_0_0,var(--ns-accent),#141316)]",
+						)}
+					/>
+				</>
+			)}
 			<div
 				className={cn(
 					"relative z-10 h-full w-full rounded-[22px] overflow-auto bg-cover ",
