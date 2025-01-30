95b971cc5d553af817e99a6ea2b7915eda6a2113
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-29 23:45:14 -0500

    added prettier tailwind and react compiler

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+// babel.config.js
+const ReactCompilerConfig = {
+	target: "18",
+};
+
+module.exports = function () {
+	return {
+		plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+	};
+};
